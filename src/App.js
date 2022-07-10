81a86b62f801b21ee44208845c747b2724737b44
go367it <satyamkumar3001@gmail.com>
2022-07-10 15:59:35 +0530

    corrected css import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
     <div className="App">
-     
+      <Container>
+        <Card>
+          <Card.Body>This is some text within a card body.</Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
