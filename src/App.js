d0e43443ee671af5284c14587bf250a4eccf11e3
go367it <satyamkumar3001@gmail.com>
2022-07-10 16:06:22 +0530

    removed react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Card>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
-      </Container>
+      
     </div>
   );
 }
